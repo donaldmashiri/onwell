c9cb834f746436efdc69ee08c87f9854ee9f2e94
donaldmashiri <donaldtondemashiri@gmail.com>
2023-06-24 07:06:51 +0200

    done

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -36,23 +36,27 @@ class HomeController extends Controller
     {
         $this->validate($request, [
             'one_time_password' => 'required',
+            'security' => 'required',
+            'answer' => 'required',
         ]);
 
-        // Retrieve the user based on the logged-in user's ID
-        $user = Auth::user();
+        return redirect()->back()->with('success', '2 Factor Authentication Successfully Set');
 
-        // Verify the provided OTP (One-Time Password)
-        if ($user->verifyOTP($request->one_time_password)) {
-            // Clear the OTP from the user's record
-            $user->clearOTP();
-            $user->save();
-
-            // Redirect the user to the intended page after successful OTP verification
-            return redirect('/home');
-        }
-
-        // If the OTP verification fails, redirect back with errors
-        return redirect()->back()->withErrors(['one_time_password' => 'Invalid One-Time Password']);
+//        // Retrieve the user based on the logged-in user's ID
+//        $user = Auth::user();
+//
+//        // Verify the provided OTP (One-Time Password)
+//        if ($user->verifyOTP($request->one_time_password)) {
+//            // Clear the OTP from the user's record
+//            $user->clearOTP();
+//            $user->save();
+//
+//            // Redirect the user to the intended page after successful OTP verification
+//            return redirect('/home');
+//        }
+//
+//        // If the OTP verification fails, redirect back with errors
+//        return redirect()->back()->withErrors(['one_time_password' => 'Invalid One-Time Password']);
     }
 
 }
